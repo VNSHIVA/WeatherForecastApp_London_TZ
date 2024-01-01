33b5768904a30506f913263533ab7777792aa849
V.N.Shivaprasad Reddy <109862126+VNSHIVA@users.noreply.github.com>
2024-01-01 22:27:21 +0530

    weatherApp is ready with London TimeZone
    London TimeZone daily weather data is shown

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, SafeAreaView, StyleSheet, Text , View, icon, ActivityIndicator  } from "react-native";
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const App = () => {
+  const [data, setData] = React.useState({"current": {"cloud": 100, "condition": {"code": 1183, "icon": "//cdn.weatherapi.com/weather/64x64/day/296.png", "text": "Light rain"}, "feelslike_c": 4.8, "feelslike_f": 40.6, "gust_kph": 19.7, "gust_mph": 12.2, "humidity": 86, "is_day": 1, "last_updated": "2024-01-01 15:45", "last_updated_epoch": 1704123900, "precip_in": 0.03, "precip_mm": 0.77, "pressure_in": 29.57, "pressure_mb": 1001, "temp_c": 7.4, "temp_f": 45.3, "uv": 2, "vis_km": 9, "vis_miles": 5, "wind_degree": 158, "wind_dir": "SSE", "wind_kph": 14, "wind_mph": 8.7}, "location": {"country": "United Kingdom", "lat": 51.52, "localtime": "2024-01-01 15:52", "localtime_epoch": 1704124366, "lon": -0.11, "name": "London", "region": "City of London, Greater London", "tz_id": "Europe/London"}});
+  const icon = "https:" + data.current.condition.icon;
+  const [loading, setLoading] = React.useState(false);
+  const date = new Date(data.location.localtime).toDateString();
+  const day = new Date(data.location.localtime).toLocaleString('default', {weekday: 'short'});
+
+
+  console.log('===================================');
+  console.log(data.current.cloud); 
+  console.log(icon);
+  console.log("https://cdn.weatherapi.com/weather/64x64/day/296.png");
+  console.log('===================================');  
+
+  React.useEffect(() => {
+    // fetch('http://api.weatherapi.com/v1/current.json?key=8645e7230aec463cb94152914240101&q=London')
+    // .then((response) => response.json())
+    // .then((data) => {
+    //    console.log('===================================');
+    //    console.log(data);
+    //    console.log('===================================');
+    // }).catch((error) => {
+    //   console.log('===================================');
+    //    console.log(error);
+    //    console.log('===================================');
+    // });
+  },[]);
+
+  return(
+   <>
+       {loading ?(
+          <ActivityIndicator size="large" color="green" />
+        ) :
+
+        ( 
+            data && (
+              <SafeAreaView style={styles.mainContainer}>
+              <Text style={styles.temp}> Temparature: {data.current.temp_c}</Text>
+              <Text style={styles.temp}> Humidity: {data.current.humidity} </Text>
+              <Text style={styles.temp}>
+                 {day}, {date}
+              </Text>
+              <Text style={styles.temp}>{data.location.name}</Text>
+              <Image source={{uri: icon}}
+               style={{width: 100, height: 100 }}
+               resizeMode="contain"
+              />
+              </SafeAreaView>
+          
+        )  
+    )}
+  </>
   );
-}
+};
 
+export default App;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  temp:{
+    fontSize: 25
   },
-});
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  mainContainer:{
+    textAlign: "center",
+    margin: 75,
+    width: 225,
+    height: 225,
+    alignContent:"center",
+    justifyContent: "space-around"
+  }
+})
